14360a5c1184cc84a886a383028137d119bec57d
Sanjin Sehic <sanjin.tr@hotmail.com>
2019-06-25 11:34:47 +0200

    Port

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const http = require('http');
 const app = express();
 
 const server = http.createServer(app);
+const port = process.env.PORT || '3000';
 
 app.get('/', (req, res, next) => {
     res.send("Hello");
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
     console.log("Server started!");
 })
